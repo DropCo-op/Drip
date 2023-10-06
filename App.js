d746ec063cc0b325b7dc78db5b389b2ce71102ac
IanFeldman <ianrfeldman@gmail.com>
2023-10-06 10:36:30 +0200

    Chris code

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Drip by drop 😎</Text>
+      <View style={styles.header}>
+        <Text style={{fontWeight: 'bold'}}>Drip By Drop</Text>
+      </View>
+      <View style={styles.map}>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          style={{width: '100%', height: '50%'}}
+        > 
+        </MapView>
+      </View>
+      <Text>drip is the best app ever</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6ECF5',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#ECCAFF',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    height: '12%',
+    paddingTop: '15%',
+    position: 'relative', 
+    top: 1,
+  },
+  map: {
+    width: '100%',
+    height: '80%',
+    position: 'relative', 
+    paddingTop: "0%",
+    top: '0%',
   },
 });
---
+++ Header.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <View style={{flexDirection:'row', margin:15 }}>
+      <Text style={{fontWeight:'bold', fontSize:30, color:'white'}}>
+        {props.name}
+      </Text>
+        
+    </View>
+  )
+}
+
+export default Header
