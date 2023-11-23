7098486d0e1df1a2ce42b819e2d278550f335f91
ChristopherYoeurng <62725833+ChristopherYoeurng@users.noreply.github.com>
2023-11-23 20:34:37 +0100

    4 tests for inputMore

--- __mocks__/mockNavigation.js
+++ __mocks__/mockNavigation.js
@@ -2,8 +2,8 @@ import * as React from "react";
 
 export const navigationRef = React.createRef();
 
-export function navigate(name, params) {
-  navigationRef.current?.navigate(name, params);
+export function navigate(name) {
+  navigationRef.current?.navigate(name);
 }
 
 export function push(name, params) {
--- test/Map.test.js
+++ test/Map.test.js
@@ -1,12 +1,70 @@
-import MapScreen from "../screens/Map";
+import InputMore from "../screens/InputMore";
 import render from "@testing-library/react-native";
-import { navigationRef } from "../__mocks__/mockNavigation.js";
+//import { navigationRef } from "../__mocks__/mockNavigation";
 
-describe("Test using the map", () => {
-  it("Normal Map Stuff", async () => {
+const routeParamsMock = {
+    "params":{
+    "Name": "name",
+    "Latitude": 1,
+    "Longitude": 1,
+    "Temperature": 2,
+    "Pressure": 1,
+    "Busyness": 1,
+    "Taste": 1,
+    "List": [],
+    }
+}
+
+const navigationRef = (props) => ({
+    navigate: ()=>{console.log("sup")},
+    ...props
+  });
+
+describe("Test using the inputMore page", () => {
+  test("Test History Text Input", async () => {
+    try{
+      const {getTestByID} = render(<InputMore navigation={ navigationRef.navigation } route={routeParamsMock}/>)
+      
+      const testHistoryTextInput = getTestByID("HistoryTextInput");
+
+      expect(testHistoryTextInput).toBeTruthy();
+    }
+    catch(err){
+        console.log(err)
+    }
+  });
+  test("Test submit button", async () => {
     try {
-      const whatever = render(<MapScreen navigation={navigationRef}/>)
-      expect(true).toBe(true);
+      const {getTestById} = render(<InputMore navigation={ navigationRef } route={routeParamsMock}/>)
+      
+      const testSubmitButton = getTestById("submitButton");
+
+      expect(testSubmitButton).toBeTruthy();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  test("Test name field", async () => {
+    try {
+      const {getTestById} = render(<InputMore navigation={ navigationRef } route={routeParamsMock}/>)
+      
+      const testNameField = getTestById("nameField");
+
+      expect(testNameField.value).toBe("name");
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  test("Test change valve count", async () => {
+    try {
+      const {getTestById} = render(<InputMore navigation={ navigationRef } route={routeParamsMock}/>)
+      
+      const testNameField = getTestById("notesOrCommentsTextField");
+
+      fireEvent.change(testNameField, { target: { value: 'this is a test' } });
+
+      expect(testNameField.value).toBe("this is a test");
     } catch (err) {
       console.log(err);
     }
