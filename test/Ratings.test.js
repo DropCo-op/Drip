7b39c47753ebbc4310db858fbbef0206d5bf4fb3
Sean Nguyen <smnguyen745@gmail.com>
2023-11-23 19:09:01 +0100

    mock folder added

--- test/Ratings.test.js
+++ test/Ratings.test.js
@@ -1,19 +1,22 @@
 import { render } from "@testing-library/react-native";
 import React from "react";
-import Ratings from "../screens/Ratings";
-import Header from "../utils/Header";
+import Ratings from "../screens/Ratings.js";
+import { navigationRef } from "../mock/mockNavigation";
 
 describe("test back button", () => {
   it("functionality", async () => {
-    const { getByTestId } = render(<Ratings />);
-    const backBtn = getByTestId("back-button");
-    expect(backBtn).toBeTruthy();
+    const { getByTestId } = render(<Ratings navigation={navigationRef}/>);
+
+    await awaitFor(() => {
+      const backBtn = getByTestId("back-button");
+      expect(backBtn).toBeTruthy();
+    });
   });
 });
 
 describe("test element rendering", () => {
-    it("title", async () => {
-        const { getByTestId } = render(<Ratings />);
-        expect(getByTestId("title").tobeTruthy());
-    });
+  it("title", async () => {
+    const { getByTestId } = render(<Ratings />);
+    expect(getByTestId("title").tobeTruthy());
+  });
 });
